4daad4db5e8c4d73ef9087e6e662338e0e3b854a
Harsh Kumar <harshkum2025@gmail.com>
2026-08-26 11:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (2.98%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.c
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.c
@@ -3,16 +3,14 @@ int countDigits(int num) {
     int lastdigit;
     int orig = num;
     int i = 0;
+
     while(num!=0){
         lastdigit = num % 10;
+        
         if(orig%lastdigit==0){
-            
             i++;
-            
         }
         num = num/10;
-
-        
     }
     return i;
     
