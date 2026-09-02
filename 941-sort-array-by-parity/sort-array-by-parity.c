470070a99b64771159a39ba967361ca4f3744051
Harsh Kumar <harshkum2025@gmail.com>
2026-09-02 11:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (99.74%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.c
+++ 941-sort-array-by-parity/sort-array-by-parity.c
@@ -1,24 +1,43 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+
 int* sortArrayByParity(int* nums, int numsSize, int* returnSize) {
-    int *num = malloc(numsSize*sizeof(int));
-    int j = 0;
 
-    for(int i = 0; i < numsSize; i++){
-        if(nums[i] % 2 == 0){
-            num[j] = nums[i];
-            j++;
+    int i = 0; int j = numsSize - 1;
+    while( i < j){
+        if( nums[i] % 2 == 0){
+            i++;
         }
-    }
+        else if(nums[j]%2 == 1){
+            j--;
+        }
+        else{
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+        
 
-    for( int i = 0; i < numsSize; i++){
-        if(nums[i] % 2 !=0){
-            num[j] = nums[i];
-            j++;
-        }
     }
     *returnSize = numsSize;
-    return num;
+    return nums;
+    // int *num = malloc(numsSize*sizeof(int));
+    // int j = 0;
+
+    // for(int i = 0; i < numsSize; i++){
+    //     if(nums[i] % 2 == 0){
+    //         num[j] = nums[i];
+    //         j++;
+    //     }
+    // }
+
+    // for( int i = 0; i < numsSize; i++){
+    //     if(nums[i] % 2 !=0){
+    //         num[j] = nums[i];
+    //         j++;
+    //     }
+    // }
+    // *returnSize = numsSize;
+    // return num;
     
 }
